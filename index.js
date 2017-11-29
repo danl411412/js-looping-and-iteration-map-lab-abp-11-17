14e978bedc2978555443238826ed083c4563f00f
danl411412 <daan.b.lee@gmail.com>
2017-11-29 23:08:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,11 +6,9 @@ function lowerCaseDrivers(arr) {
 }
 
 function nameToAttributes(arr) {
-  let newArr = [];
   return arr.map(function(fullName) {
     let spaceIndex = fullName.indexOf(" ");
     console.log(spaceIndex);
-    newArr.push(Object.assign({}, {firstName: fullName.slice(0, spaceIndex)}, {lastName: fullName.slice(spaceIndex+1)}));
-    return newArr;
+    Object.assign({}, {firstName: fullName.slice(0, spaceIndex)}, {lastName: fullName.slice(spaceIndex+1)});
   })
 }
